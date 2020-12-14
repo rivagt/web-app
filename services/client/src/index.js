7f6de8e8fd8f83c48bd6eb6bb722019778afda62
rivagt <rivaldogarcia@upeu.edu.pe>
2020-12-14 16:12:01 +0000

    travis

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,26 +1,48 @@
-import React, { Component } from 'react';  // nuevo
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
-
-// nuevo
+import AddUser from './components/AddUser';
 
 class App extends Component {
-  // new
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '',
     };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   };
-  // new
+
+  handleChange(event){
+    const obj = {};
+    obj [event.target.name] = event.target.value;
+    this.setState (obj);
+  }
+
   componentDidMount() {
     this.getUsers();
   };
-  getUsers() {
-    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-    .then((res) => { this.setState({ users: res.data.data.users }); })
+  addUser(event){
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    .then((res) => {
+      this.getUsers();  // nuevo
+      this.setState({ username: '', email: '' });  // nuevo
+    })
     .catch((err) => { console.log(err); });
+  };
+
+  getUsers() {
+  axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+  .then((res) => { this.setState({ users: res.data.data.users }); })
+  .catch((err) => { console.log(err); });
   };
   render() {
     return (
@@ -31,6 +53,13 @@ class App extends Component {
               <br/>
               <h1 className="title is-1">All Users</h1>
               <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />  {/* nuevo */}
+              <br/><br/>  {/* nuevo */}
               <UsersList users={this.state.users}/>
             </div>
           </div>
@@ -39,7 +68,6 @@ class App extends Component {
     )
   }
 };
-
 
 ReactDOM.render(
   <App />,
